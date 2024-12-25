ca122b2ad8ece854de7c861e369154eb82ed9fdd
DarinelGuillen <darinel.escobar@outlook.com>
2024-12-25 14:01:01 -0600

    📦 build(package.json): add deployment script and gh-pages dependency
    - add "deploy" script for GitHub Pages deployment
    - include "gh-pages" dependency for deployment support
    
    🔧 chore(pnpm-lock.yaml): update lockfile with gh-pages and related packages
    
    - update pnpm-lock.yaml with gh-pages and related dependency versions
    - ensure consistent package versions and integrity checks
    
    🔧 chore(tsconfig.tsbuildinfo): add tsconfig tsbuildinfo for composite build
    
    - track TypeScript incremental compilation information with tsbuildinfo
    
    🔧 chore(vite.config.ts): enhance Vite configuration for project build
    
    - update base URL for GitHub Pages deployment
    - add output directory and source map options for optimized build

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,16 @@ import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Cambia el valor de base dependiendo del nombre del repositorio
+  base: "/react-vite-shadcn-ui-template/", // Ejemplo: "/react-vite-shadcn-ui-template/" o "/" si es DarinelGuillen.github.io
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": resolve(__dirname, "./src"), // Atajo para facilitar los imports
     },
+  },
+  build: {
+    outDir: "dist", // Carpeta donde se generarán los archivos estáticos
+    sourcemap: true, // Opcional: Incluye mapas de fuente para facilitar la depuración
   },
 });
